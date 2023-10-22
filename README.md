38f102dc8308bf9b4cc18301cb2742c0ec9c9847
vulinux <vulinux0219@gmail.com>
2023-10-22 13:10:15 +0200

    Added top.vhd
    Added top.vhd and minor change to README.md

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # hamster-galaxy-repo
 This repo holds all interdeimensional hamsters.
+I've seen them myaan!
---
+++ top.vhd
@@ -0,0 +1,8 @@
+entity top is
+    i_sys_clk : in std_logic
+end top;
+
+architecture top_behav of top is
+begin
+    
+end top_behav;
